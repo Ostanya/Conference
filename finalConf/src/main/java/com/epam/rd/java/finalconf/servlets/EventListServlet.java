620e8cdfab33bfc5fefdbdcdffdd521dcca39b0c
Osttanya <ostania.tania@gmail.com>
2021-09-05 19:43:54 +0300

    add eventlistservlet

---
+++ finalConf/src/main/java/com/epam/rd/java/finalconf/servlets/EventListServlet.java
@@ -0,0 +1,24 @@
+package com.epam.rd.java.finalconf.servlets;
+
+import com.epam.rd.java.finalconf.entity.repository.EventRepository;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/event-list")
+public class EventListServlet extends HttpServlet {
+
+    private EventRepository eventRepository;
+
+    @Override
+    public void init() {eventRepository = EventRepository.getEventRepository();}
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/view/event-list.jsp");
+        request.setAttribute("events", eventRepository.all());
+        rd.forward(request, response);
+    }
+}
