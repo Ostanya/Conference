6a435c81b4bc508a3a58ec227ee139cde3ac9e90
Osttanya <ostania.tania@gmail.com>
2021-09-06 00:25:12 +0300

    add creator of events

--- finalConf/src/main/java/com/epam/rd/java/finalconf/servlets/events/EventCreateServlet.java
+++ finalConf/src/main/java/com/epam/rd/java/finalconf/servlets/events/EventCreateServlet.java
@@ -1,18 +1,32 @@
+package com.epam.rd.java.finalconf.servlets.events;
+
+import com.epam.rd.java.finalconf.entity.Event;
+import com.epam.rd.java.finalconf.entity.repository.EventRepository;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "EventCreateServlet", value = "/EventCreateServlet")
+@WebServlet("/create-event")
 public class EventCreateServlet extends HttpServlet {
+    EventRepository eventRepository;
+
+    @Override
+    public void init() {
+        eventRepository = EventRepository.getEventRepository();
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/view/create-event.jsp");
+        requestDispatcher.forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        String nName = request.getParameter("nname");
+        String tName = request.getParameter("tname");
+        eventRepository.create(new Event(nName, tName));
+        doGet(request, response);
     }
 }
