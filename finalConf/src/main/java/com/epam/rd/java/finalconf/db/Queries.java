9079f695dd8c73e5e3b124b96f9e01fbf592a45a
Osttanya <ostania.tania@gmail.com>
2021-09-16 19:27:56 +0300

    refactor of queries

---
+++ finalConf/src/main/java/com/epam/rd/java/finalconf/db/Queries.java
@@ -0,0 +1,29 @@
+package com.epam.rd.java.finalconf.db;
+
+public class Queries {
+    public static final String FIND_BY_ID_QUERY = "SELECT * FROM users WHERE users.id = ?";
+    public static final String FIND_BY_USER_EMAIL_QUERY = "SELECT * FROM users WHERE users.email = ?";
+    public static final String SAVE_USER_QUERY = "INSERT INTO users (first_name, last_name, email, password, role) VALUES (?, ?, ?, ?, ?)";
+    public static final String DELETE_USER_BY_ID = "DELETE FROM users WHERE id = ?";
+    public static final String FIND_ALL_USERS_QUERY = "SELECT * FROM users";
+    public static final String UPDATE_USER_QUERY = "UPDATE users SET first_name = ?, last_name = ?, password = ?, enabled = ?, role = ? WHERE id = ?";
+    public static final String UPDATE_USER_BY_ROLE_QUERY = "UPDATE users set role = ? WHERE id = ?";
+
+    public static final String FIND_EVENT_BY_ID_QUERY = "SELECT * FROM events WHERE id = ?";
+    public static final String FIND_ALL_EVENTS_QUERY = "SELECT * FROM events";
+    public static final String SAVE_EVENT_QUERY = "INSERT INTO events (title, scheduled_date) VALUES (?, ?)";
+    public static final String SQL_FIND_FROM_EVENT_IDS = "SELECT * FROM events WHERE id IN (%s)";
+
+    public static final String SAVE_PARTICIPANTS_QUERY = "INSERT INTO participants (is_speaker, nick_name, user_id, event_id) VALUES (?, ?, ?, ?)";
+    public static final String FIND_ALL_PARTICIPANTS_QUERY = "SELECT * FROM participants";
+    public static final String FIND_PARTICIPANT_BY_ID_QUERY = "SELECT * FROM participants WHERE id = ?";
+    public static final String FIND_BY_USER_ID_FROM_PARTICIPANT_QUERY = "SELECT * FROM participants WHERE user_id = ?";
+
+    public static final String FIND_BY_EVENT_ID_FROM_TOPIC_QUERY = "SELECT * FROM topics WHERE event_id = ?";
+    public static final String DELETE_EVENT_BY_ID_QUERY = "DELETE FROM events WHERE id = ?";
+    public static final String UPDATE_EVENT_QUERY = "UPDATE events SET title = ?, scheduled_date = ? WHERE id = ?";
+
+    public static final String SAVE_TOPIC_QUERY = "INSERT INTO topics (title, event_id) VALUES (?, ?)";
+    public static final String FIND_TOPIC_BY_ID_QUERY = "SELECT * FROM topics WHERE id = ?";
+    public static final String UPDATE_TOPIC_QUERY = "UPDATE topics SET speaker_id = ?, event_id = ? WHERE id = ?";
+}
